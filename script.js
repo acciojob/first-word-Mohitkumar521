fe0bd3531d16b1c7327e0c5a390dfaa87c15db6e
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-26 13:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(str) {
-  if (str === "" || !str.includes(" ")) {
+  if (str === "" || !str.includes('')) {
     // If the input string is empty or does not contain any space, return the entire string
     return str;
   } else {
     // Find the index of the first space
-    const spaceIndex = str.indexOf(" ");
+    const spaceIndex = str.indexOf('');
     
     // Extract and return the first word (characters up to the first space)
     return str.substring(0, spaceIndex);
